446fd04dbe2e72872a18e0dcc6f5b3c0bca1eab8
Andrew Lee <andrew12678@gmail.com>
2021-03-21 10:02:47 +1100

    Removed sklearn dependency and added a numpy implementation of k-fold stratified

--- utils.py
+++ utils.py
@@ -25,31 +25,60 @@ def create_kfold_stratfied_cross_validation(
     X_test: np.ndarray,
     y_test: np.ndarray,
     k: int = 5,
+    seed: int = 18,
 ):
     """
-    Combines the given train-test dataset and creates the k-fold cross validation folds for them
+    Combines the given train-test dataset and creates the k-fold cross validation folds for them without
+    sklearn
     Args:
         X_train (np.ndarray): the training features
         y_train (np.ndarray): the training labels
         X_test (np.ndarray): the testing features
         y_test (np.ndarray): the testing labels
         k (int): the number of cross validation folds
+        seed (int): the random seed for reproducibility
 
     Returns:
         A list where containing the k-folds (could be optimised later to only return the indices)
     """
     X_all = np.concatenate([X_train, X_test])
     y_all = np.concatenate([y_train, y_test])
-    skf = StratifiedKFold(n_splits=k)
-    skf.get_n_splits(X_all, y_all)
+
+    # Get the number of elements per group
+    group_counts = np.bincount(y_all.flatten())
+
+    # Gets the original indices of each group
+    # e.g. [2,0,3,1,1] -> [[1],[3,4],[0],[2],[]]
+    original_group_indices = np.split(
+        y_all.flatten().argsort(), np.cumsum(group_counts)
+    )
+
+    # Splits the grouped indices of each element into k
+    group_fold_indices = []
+    for group_count in group_counts:
+        group_indices = np.arange(group_count)
+        np.random.seed(seed)
+        np.random.shuffle(group_indices)
+        group_fold_indices.append(np.array_split(group_indices, k))
+
+    # Generate k-folds with some output as sklearn.StratifiedKFold
     splits = []
-    for train_index, test_index in skf.split(X_all, y_all):
-        splits.append(
-            (
-                X_all[train_index],
-                y_all[train_index],
-                X_all[test_index],
-                y_all[test_index],
-            )
+    for fold_index in range(k):
+        # For each fold we consider get the relevant group indices we split into k subgroups
+        # and look them up in the original
+        test_indices = np.concatenate(
+            [
+                original_group_indices[group_index][group[fold_index]]
+                for group_index, group in enumerate(group_fold_indices)
+            ]
         )
+        # Uses masks to allow negation
+        mask = np.zeros(X_all.shape[0], dtype=bool)
+        mask[test_indices] = True
+        X_train = X_all[~mask]
+        y_train = y_all[~mask]
+        X_test = X_all[mask]
+        y_test = y_all[mask]
+        splits.append((X_train, y_train, X_test, y_test))
+
     return splits
