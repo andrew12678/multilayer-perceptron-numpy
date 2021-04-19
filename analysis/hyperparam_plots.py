341e88a0e9441361248a5b3b984e7bd32ebfea5c
Daniel <brauny_13@hotmail.com>
2021-04-19 16:19:18 +1000

    Added learning curve and ablation plot capabilities

--- analysis/hyperparam_plots.py
+++ analysis/hyperparam_plots.py
@@ -11,14 +11,48 @@ def write_hyperparams_table(data):
     acc_norm = (df["accuracy"] - df["accuracy"].mean()) / df["accuracy"].std()
     f1_macro_norm = (df["f1_macro"] - df["f1_macro"].mean()) / df["f1_macro"].std()
     # Combine metrics by taking their sum (we use a negative for loss since we aim to minimise it)
-    df["validation_score"] = acc_norm - loss_norm + f1_macro_norm
+    # df["validation_score"] = acc_norm - loss_norm + f1_macro_norm
 
-    df_sorted = df.sort_values("validation_score", ascending=False, ignore_index=True)
-    print(df_sorted)
+    # df = df.sort_values("validation_score", ascending=False, ignore_index=True)
+    df = df.sort_values("loss", ascending=True, ignore_index=True)
+    df = df[[
+        'batch_size', 'num_epochs', 'learning_rate', 'weight_decay', 'momentum', 'num_hidden',
+        'hidden_size', 'dropout_rate', 'batch_normalisation', 'loss', 'accuracy',
+        'f1_macro',
+        # 'validation_score'
+    ]]
+    df = df.rename(columns={
+        'batch_size': 'b_size', 'num_epochs': 'epochs', 'learning_rate': 'lr', 'weight_decay': 'wd',
+        'momentum': 'mom', 'num_hidden': 'n_h', 'hidden_size': 'h_size', 'dropout_rate': 'drp',
+        'batch_normalisation': 'bn', 'loss': 'loss', 'accuracy': 'acc', 'f1_macro': 'f1',
+        # 'validation_score': 'score'
+    })
+    df.index += 1
+    df['loss'] = df['loss'].round(4)
+    # df[['acc', 'f1', 'score']] = df[['acc', 'f1', 'score']].round(3)
+    df[['acc', 'f1']] = df[['acc', 'f1']].round(3)
+    df['bn'] = df['bn'].map({False: 'N', True: 'Y'})
+
+    # Running manually without multiprocessing we get different results. Use these results to
+    # replace the top 3 in the grid search for replicability
+    # manual_runs = [
+    #     {'loss': 'cross_entropy', 'optimiser': 'sgd', 'batch_size': 64, 'num_epochs': 100,
+    #     'learning_rate': 0.01, 'weight_decay': 0.0001, 'momentum': 0.5, 'num_hidden': 4,
+    #     'hidden_size': 128, 'activation': 'relu', 'dropout_rate': 0, 'batch_normalisation': False},
+    #     {'loss': 'cross_entropy', 'optimiser': 'sgd', 'batch_size': 64, 'num_epochs': 100,
+    #     'learning_rate': 0.01, 'weight_decay': 0.0001, 'momentum': 0.5, 'num_hidden': 3,
+    #     'hidden_size': 128, 'activation': 'relu', 'dropout_rate': 0, 'batch_normalisation': False},
+    #     {'loss': 'cross_entropy', 'optimiser': 'sgd', 'batch_size': 64, 'num_epochs': 100,
+    #     'learning_rate': 0.01, 'weight_decay': 0.0001, 'momentum': 0.5, 'num_hidden': 2,
+    #     'hidden_size': 128, 'activation': 'relu', 'dropout_rate': 0, 'batch_normalisation': False},
+    # ]
+
+
+    print(df.head(30))
     # Save the top 15 values to a latex table
-    # Note that these tables are currently too big for report (even in single column appendix)
+    # Note that we overwrite the top 3 values with values obtained without multiprocessing.
     with open("analysis/hyperparam_table.text", "w") as f:
-        f.write(df_sorted[:15].to_latex(index=False))
+        f.write(df[:15].to_latex(index=True))
 
 
 def arg_parser():
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import itertools
 import argparse
 from multiprocessing import Pool
 import json
+import matplotlib.pyplot as plt
+import os
 
 from src.trainer.trainer import Trainer
 from src.utils.io import load_directory
@@ -13,16 +15,17 @@ from src.network.mlp import MLP
 from src.utils.ml import one_hot, create_stratified_kfolds, create_layer_sizes
 
 
-def run(args):
-    # Import training and test data
-    X_train, y_train, X_test, y_test = load_directory("data")
+def run(args, hyperparams, X_train, y_train, X_test, y_test):
 
     # Define the number of classes
     n_classes = len(np.unique(y_train))
 
+    # Since we only support training on a single set of hyperparams in this function, extract the
+    # hyperparam dictionary
+    p = hyperparams[0]
     # Load hyperparameters from file
-    with open(args.config, "r") as f:
-        p = yaml.safe_load(f)[args.hyperparams]
+    # with open(args.config, "r") as f:
+    #     p = yaml.safe_load(f)[args.hyperparams]
 
     # Create a list of tuples indicating the size of each network layer
     layer_sizes = create_layer_sizes(
@@ -59,7 +62,8 @@ def run(args):
     )
 
     # Train model
-    train_loss = trainer.train()
+    _ = trainer.train()
+    train_loss = trainer.validation(X=X_train, y=y_train)["loss"]
 
     # Test model
     test_metrics = trainer.validation(X=X_test, y=y_test)
@@ -80,8 +84,8 @@ def run_experiment(args):
     # Set the dropout rate for each layer, keeping the first layer as 0
     dropout_rates = [0] + [params["dropout_rate"]] * params["num_hidden"]
 
-    # Instantiate accumulated trackers of loss, accuracy, and f1_macro
-    metrics = {"loss": 0, "accuracy": 0, "f1_macro": 0}
+    # Instantiate accumulated trackers of training loss, cv loss, accuracy, and f1_macro
+    metrics = {"train_loss": 0, "cv_loss": 0, "accuracy": 0, "f1_macro": 0}
 
     # Train and test on each k-fold split
     for k, (X_train_k, y_train_k, X_val_k, y_val_k) in enumerate(splits):
@@ -108,56 +112,59 @@ def run_experiment(args):
         )
 
         # Train model on training set
-        train_loss = trainer.train()
+        regularized_train_loss = trainer.train()
 
         # Kill search if training loss is nan
-        if np.isnan(train_loss):
-            return {**params, "loss": np.nan, "accuracy": np.nan, "f1_macro": np.nan}
+        if np.isnan(regularized_train_loss):
+            return {**params, "train_loss": np.nan, "cv_loss": np.nan, "accuracy": np.nan,
+                    "f1_macro": np.nan}
 
+        # Get training loss when the model is in "test" model (e.g. no dropout)
+        metrics["train_loss"] += trainer.validation(X=X_train_k, y=y_train_k)["loss"]
         # Extract validation loss and predicted labels
         val_results = trainer.validation(X=X_val_k, y=y_val_k)
+        val_results["cv_loss"] = val_results["loss"]
 
-        # Accumulate loss, accuracy, f1_macro
+        # Accumulate losses, accuracy, f1_macro
         for met in metrics:
-            metrics[met] += val_results[met]
+            if met != "train_loss":
+                metrics[met] += val_results[met]
 
     # Divide the total accumualted metrics by the number of folds
     for met in metrics:
         metrics[met] /= len(splits)
 
     print(f"Trained on params: {params}")
-    print(f"Overall cross-validation loss: {metrics['loss']}")
+    print(f"Overall cross-validation loss: {metrics['cv_loss']}")
     summary = {**params, **metrics}
     return summary
 
 
-def run_kfolds(args):
-    # Load training and test data
-    X_train, y_train, X_test, y_test = load_directory("data")
-
+def run_kfolds(args, hyperparams, X_train, y_train, write=True):
     # Get number of classes
     n_classes = len(np.unique(y_train))
 
     # Define number of cross-validation folds
     num_folds = 5
 
-    # Load hyperparameters from file
-    with open(args.config, "r") as f:
-        hyperparam_grid = yaml.safe_load(f)[args.hyperparams]
-    # print("Hyperparameter setting ranges", hyperparam_grid)
+    # # Load hyperparameters from file
+    # with open(args.config, "r") as f:
+    #     hyperparam_grid = yaml.safe_load(f)[args.hyperparams]
+    # # print("Hyperparameter setting ranges", hyperparam_grid)
 
-    # Setup grid search by enumerating all possible combination of parameters in hyperparam_grid
-    # e.g. {'batch_size': [24, 48], 'num_epochs': [2]} -> [{'batch_size': 24, 'num_epochs': 2}
-    #                                                     {'batch_size': 48, 'num_epochs': 2}]
-    keys, values = zip(*hyperparam_grid.items())
-    param_list = [dict(zip(keys, v)) for v in itertools.product(*values)]
+    # # Setup grid search by enumerating all possible combination of parameters in hyperparam_grid
+    # # e.g. {'batch_size': [24, 48], 'num_epochs': [2]} -> [{'batch_size': 24, 'num_epochs': 2}
+    # #                                                     {'batch_size': 48, 'num_epochs': 2}]
+    # keys, values = zip(*hyperparam_grid.items())
+    # param_list = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
     # Create training-validation splits
     splits = create_stratified_kfolds(X_train, y_train, num_folds)
 
     pool_args = []
 
-    for p in param_list:
+    # Check whether we are training on multiple sets of hyperparams
+    for p in hyperparams:
         pool_args.append((p, X_train, y_train, n_classes, splits))
 
     summary = []
@@ -169,13 +176,112 @@ def run_kfolds(args):
         for p in pool_args:
             summary.append(run_experiment(p))
 
-    # Write results to unique file
-    results_file = (
-        f"results/{args.hyperparams}_{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
-    )
-    with open(results_file, "w+") as f:
-        json.dump(summary, f)
+    if write:
+        # Write results to unique file
+        results_file = (
+            f"results/{args.hyperparams}_{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
+        )
+        with open(results_file, "w+") as f:
+            json.dump(summary, f)
 
+    return summary
+
+def plot_learning_curves(args, hyperparams, X_train, y_train):
+    plot_dir = "analysis/plots"
+    # Make the plot dir if it doesn't exist
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+    # Randomly shuffle all data
+    idxs = list(range(len(X_train)))
+    np.random.shuffle(idxs)
+    X_train_shuffled = X_train[idxs]
+    y_train_shuffled = y_train[idxs]
+
+    train_losses = []
+    cv_losses = []
+    increment = 10000
+    num_examples = list(range(increment, len(X_train) + 1, increment))
+
+    # Number of examples to increase for each datapoint (e.g. 5000 -> 5000, 10000, ..., 50000)
+    # increment = 10000
+    for i in num_examples:
+        X = X_train_shuffled[:i]
+        y = y_train_shuffled[:i]
+        # Get kfolds scores for this set of examples
+        summary = run_kfolds(args, hyperparams, X, y, write=False)
+        train_losses.append(summary[0]["train_loss"])
+        cv_losses.append(summary[0]["cv_loss"])
+    print(train_losses)
+    print(cv_losses)
+
+    # Example losses
+    # train_losses = [0.15476380272853724, 0.14724771435319572, 0.14368380648098447, 0.14226972405205962, 0.1410810492802653, 0.1396173106561778, 0.13858820971661207, 0.1376052818842732, 0.13726238212764735, 0.13726352644214462]
+    # cv_losses = [0.17368803855208034, 0.16148927089504553, 0.15650455458394166, 0.15298698865546764, 0.1503882746192336, 0.14806578619777597, 0.14688472539691927, 0.14503313948724825, 0.1441868445946898, 0.1441555267272751]
+
+    plt.plot(num_examples, train_losses, "-o", label="Training Loss")
+    plt.plot(num_examples, cv_losses, "-o", label="Validation Loss")
+    plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.05))
+    plt.xlabel("Number of examples")
+    plt.ylabel("Cross Entropy loss")
+    # The title will be better in latex
+    # plt.title("Learning curves for best model")
+    plt.savefig(f"{plot_dir}/lc_{args.hyperparams}_{datetime.now().strftime('%Y%m%d%H%M%S')}.png")
+    plt.show()
+
+def plot_ablation(args, hyperparams, X_train, y_train):
+    """
+    In this ablation analysis, we run the following for each hyperparameter:
+    1. Select a hyperparameter to vary and plot
+    2. Fix all other hyperparameters using those from the best model
+    3. Make a plot showing how the errors change overtime w.r.t the selected hyperparameter
+    """
+
+    plot_dir = "analysis/plots"
+    # Make the plot dir if it doesn't exist
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+
+    # We assume that hyperparams is a list of length 1 to run this function
+    hyperparams = hyperparams[0]
+
+    # Load ablation hyperparameters to search over
+    with open(args.config, "r") as f:
+        abl_hyperparams = yaml.safe_load(f)[args.ablation_hyperparams]
+
+    for param, values in abl_hyperparams.items():
+        # if param not in ["dropout_rate", "batch_normalisation"]:
+        if param not in ["batch_normalisation", "dropout_rate", "momentum"]:
+            continue
+        # Ignore hyperparameters that we don't vary
+        if param not in ["loss", "optimiser", "activation"]:
+            # Iterate through all possible options for the hyperparameter
+            train_losses = []
+            cv_losses = []
+            for val in values:
+                # Create new dictionary of hyperparams with all values from hyperparams except for val
+                new_hyperparams = [{**hyperparams, param: val}]
+                summary = run_kfolds(args, new_hyperparams, X_train, y_train, write=False)
+                train_losses.append(summary[0]["train_loss"])
+                cv_losses.append(summary[0]["cv_loss"])
+
+            plt.figure()
+            plt.style.use('ggplot')
+            if param == "batch_normalisation":
+                # Position of bars on x-axis
+                ind = np.arange(len(values))
+                # Width of a bar
+                width = 0.3
+                plt.bar(ind, train_losses , width, label="Training Loss")
+                plt.bar(ind + width, cv_losses, width, label="Validation Loss")
+                plt.xticks(ind + width / 2, values)
+            else:
+                plt.plot(values, train_losses, "-o", label="Training Loss")
+                plt.plot(values, cv_losses, "-o", label="Validation Loss")
+
+            plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.05))
+            plt.xlabel(param)
+            plt.ylabel("Cross Entropy loss")
+            plt.savefig(f"{plot_dir}/{param}_{args.hyperparams}_{datetime.now().strftime('%Y%m%d%H%M%S')}.png")
 
 def arg_parser():
     parser = argparse.ArgumentParser()
@@ -202,6 +308,19 @@ def arg_parser():
     parser.add_argument(
         "-s", "--seed", default=42, type=int, help="Random seed used for experiment"
     )
+    parser.add_argument(
+        "-lc", "--learning_curves", default=0, type=int, help="Whether to plot learning curves"
+    )
+    parser.add_argument(
+        "-a", "--ablation", default=0, type=int, help="Whether to plot ablation analysis"
+    )
+    parser.add_argument(
+        "-ahy",
+        "--ablation_hyperparams",
+        default="grid-ablation",
+        type=str,
+        help="Name of grid for ablation analysis",
+    )
     args = parser.parse_args()
     return args
 
@@ -214,9 +333,30 @@ if __name__ == "__main__":
     # Set a random seed to reproduce results
     np.random.seed(args.seed)
 
-    if args.kfolds:
-        run_kfolds(args)
+    # Import training and test data
+    X_train, y_train, X_test, y_test = load_directory("data")
+
+    # Load hyperparameters from file
+    with open(args.config, "r") as f:
+        hyperparams = yaml.safe_load(f)[args.hyperparams]
+
+    # Check if hyperparam values are in a list format (used for kfolds validation and plotting)
+    if isinstance(hyperparams["hidden_size"], list):
+        # Setup grid search by enumerating all possible combination of parameters in hyperparams
+        # e.g. {'batch_size': [24, 48], 'num_epochs': [2]} -> [{'batch_size': 24, 'num_epochs': 2}
+        #                                                     {'batch_size': 48, 'num_epochs': 2}]
+        keys, values = zip(*hyperparams.items())
+        hyperparams = [dict(zip(keys, v)) for v in itertools.product(*values)]
     else:
-        run(args)
+        hyperparams = [hyperparams]
+
+    if args.kfolds:
+        run_kfolds(args, hyperparams, X_train, y_train)
+    elif args.learning_curves:
+        plot_learning_curves(args, hyperparams, X_train, y_train)
+    elif args.ablation:
+        plot_ablation(args, hyperparams, X_train, y_train)
+    else:
+        run(args, hyperparams, X_train, y_train, X_test, y_test)
 
     print("Run time: ", time.time() - start_time)
